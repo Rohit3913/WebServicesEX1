47d9a92a834b769e632c60315d31e344ddaed759
Rohit3913 <crohit3913@gmail.com>
2016-11-02 23:59:59 +0530

    Update AuthGeneratorImp.java

--- Example4/src/main/java/com/rohit/codefirst/AuthGeneratorImp.java
+++ Example4/src/main/java/com/rohit/codefirst/AuthGeneratorImp.java
@@ -3,13 +3,20 @@ package com.rohit.codefirst;
 public class AuthGeneratorImp implements AuthGenerator {
 
 	public AuthOutput getOTP(AuthInput input) {
+	
 		// TODO Auto-generated method stub
 		int input1= (int) (input.getName().length()*Math.random());
-		Double input2=Double.parseDouble(input.getPhoneNumber());
-		String output=String.valueOf(input1*input2);
+		String[] split = input.getPhoneNumber().split("");
+		int sum=0;
+		for (String value : split) {
+			sum=Integer.valueOf(value)+sum;
+		}
+		
+		String output=String.valueOf((int)input1*sum);
 		AuthOutput authOutput = new AuthOutput();
 		authOutput.setOtp(output);
 		return authOutput;
+	
 	}
 
 }
